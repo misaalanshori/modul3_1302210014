398c0b1f4fdbd3160f2d2c0337dee825463c9b47
Hasnannn <hasnanh.7113@gmail.com>
2023-03-07 16:02:06 +0700

    menambahkan layout GUI kalkulator

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,15 @@ namespace MODUL3_1302210014
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
