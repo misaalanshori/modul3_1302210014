020e65b00ba8e79d8a4af3114bd22d4afc92f400
Muhammad Isa Al Anshori <isabungsu@gmail.com>
2023-03-07 16:49:15 +0700

    sedikit koreksi

--- Form1.cs
+++ Form1.cs
@@ -17,58 +17,59 @@ namespace MODUL3_1302210014
 
         private void button3_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 0;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "0";
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 1;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "1";
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 2;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "2";
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 3;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "3";
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 4;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "4";
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 5;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "5";
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 6;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "6";
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 7;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "7";
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 8;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "8";
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            this.label1.Text += 9;
+            this.label1.Text = Convert.ToInt32(this.label1.Text) + "9";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             sum += Convert.ToInt32(this.label1.Text);
             this.label1.Text = "" + sum;
+            sum = 0;
         }
     }
 }
